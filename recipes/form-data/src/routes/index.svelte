89edbdf7a9c7834c12e6855cbc6cd39e8fd085bb
Misha Rodshtein <13717428+rodshtein@users.noreply.github.com>
2022-05-22 21:49:18 +1000

    Update index.svelte

--- recipes/form-data/src/routes/index.svelte
+++ recipes/form-data/src/routes/index.svelte
@@ -1,14 +1,14 @@
 <script>
-	async function submit() {
-    let data = new FormData(this); // `this` - it's form body 
-    let response = await fetch('/', { 
-      method: 'POST',  
-      headers: { 'accept': 'application/json'},  
-      // ↑ we need the `Header` to receive endpoint response instead html page
-      body: data 
-    });
-    alert(await response.text()) // get response
-	}
+async function submit() {
+  let data = new FormData(this); // `this` - it's form body 
+  let response = await fetch('/', { 
+    method: 'POST',
+	  headers: { 'accept': 'application/json'},  
+    // ↑ we need the `Header` to receive endpoint response instead html page
+    body: data 
+  });
+  alert(await response.text()) // get response
+}
 </script>
 
 <form on:submit|preventDefault={submit}>
