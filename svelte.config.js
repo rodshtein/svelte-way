980d2285f70ac03967c550750912b76df1e18c0c
Misha Zamkevich <uwwcom@gmail.com>
2022-05-22 19:58:18 +1000

    update dep

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 
+const dir = process.env.APP_DIR;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		files: {
+      assets: `recipes/${dir}/static`,
+      hooks: `recipes/${dir}/src/hooks`,
+      lib: `recipes/${dir}/src/lib`,
+      routes: `recipes/${dir}/src/routes`,
+      serviceWorker: `recipes/${dir}/src/service-worker`,
+      template: `recipes/${dir}/src/app.html`
+    },
 	}
 };
 
